bc24cf2897a517a246465a825feea5f123f6b425
sg.chernov <sg.chernov>
2016-11-05 18:48:47 +0300

    форматирование файла

--- Введение в статистическое обучение/Chapter_6.R
+++ Введение в статистическое обучение/Chapter_6.R
@@ -96,65 +96,65 @@ for(j in 1:k){
 mean.cv.errors = apply(cv.errors, 2, mean)
 mean.cv.errors
 par(mfrow=c(1, 1))
-plot(mean.cv.errors,type='b')
+plot(mean.cv.errors, type='b')
 reg.best = regsubsets(Salary~., data=Hitters, nvmax=19)
 coef(reg.best, 11)
 
 
 # Chapter 6 Lab 2: Ridge Regression and the Lasso
 # ГРЕБНЕВАЯ РЕГРЕССИЯ И ЛАССО
-x=model.matrix(Salary~.,Hitters)[,-1]
-y=Hitters$Salary
+x = model.matrix(Salary~., Hitters)[, -1]
+y = Hitters$Salary
 
 # Ridge Regression
 
 library(glmnet)
-grid=10^seq(10,-2,length=100)
-ridge.mod=glmnet(x,y,alpha=0,lambda=grid)
+grid = 10^seq(10, -2, length=100)
+ridge.mod = glmnet(x, y, alpha=0, lambda=grid)
 dim(coef(ridge.mod))
 ridge.mod$lambda[50]
-coef(ridge.mod)[,50]
-sqrt(sum(coef(ridge.mod)[-1,50]^2))
+coef(ridge.mod)[, 50]
+sqrt(sum(coef(ridge.mod)[-1, 50]^2))
 ridge.mod$lambda[60]
-coef(ridge.mod)[,60]
-sqrt(sum(coef(ridge.mod)[-1,60]^2))
-predict(ridge.mod,s=50,type="coefficients")[1:20,]
+coef(ridge.mod)[, 60]
+sqrt(sum(coef(ridge.mod)[-1, 60]^2))
+predict(ridge.mod, s=50, type="coefficients")[1:20, ]
 set.seed(1)
-train=sample(1:nrow(x), nrow(x)/2)
-test=(-train)
-y.test=y[test]
-ridge.mod=glmnet(x[train,],y[train],alpha=0,lambda=grid, thresh=1e-12)
-ridge.pred=predict(ridge.mod,s=4,newx=x[test,])
+train = sample(1:nrow(x), nrow(x)/2)
+test = (-train)
+y.test = y[test]
+ridge.mod = glmnet(x[train,], y[train], alpha=0, lambda=grid, thresh=1e-12)
+ridge.pred = predict(ridge.mod, s=4, newx=x[test,])
 mean((ridge.pred-y.test)^2)
 mean((mean(y[train])-y.test)^2)
-ridge.pred=predict(ridge.mod,s=1e10,newx=x[test,])
+ridge.pred = predict(ridge.mod, s=1e10, newx=x[test,])
 mean((ridge.pred-y.test)^2)
-ridge.pred=predict(ridge.mod,s=0,newx=x[test,],exact=T)
+ridge.pred = predict(ridge.mod, s=0, newx=x[test,], exact=T)
 mean((ridge.pred-y.test)^2)
 lm(y~x, subset=train)
-predict(ridge.mod,s=0,exact=T,type="coefficients")[1:20,]
+predict(ridge.mod, s=0, exact=T, type="coefficients")[1:20, ]
 set.seed(1)
-cv.out=cv.glmnet(x[train,],y[train],alpha=0)
+cv.out = cv.glmnet(x[train, ], y[train], alpha=0)
 plot(cv.out)
-bestlam=cv.out$lambda.min
+bestlam = cv.out$lambda.min
 bestlam
-ridge.pred=predict(ridge.mod,s=bestlam,newx=x[test,])
-mean((ridge.pred-y.test)^2)
-out=glmnet(x,y,alpha=0)
-predict(out,type="coefficients",s=bestlam)[1:20,]
+ridge.pred = predict(ridge.mod, s=bestlam, newx=x[test, ])
+mean((ridge.pred - y.test)^2)
+out = glmnet(x,y,alpha=0)
+predict(out, type="coefficients", s=bestlam)[1:20, ]
 
 # The Lasso
 
-lasso.mod=glmnet(x[train,],y[train],alpha=1,lambda=grid)
+lasso.mod = glmnet(x[train, ], y[train], alpha=1, lambda=grid)
 plot(lasso.mod)
 set.seed(1)
-cv.out=cv.glmnet(x[train,],y[train],alpha=1)
+cv.out = cv.glmnet(x[train, ], y[train], alpha=1)
 plot(cv.out)
-bestlam=cv.out$lambda.min
-lasso.pred=predict(lasso.mod,s=bestlam,newx=x[test,])
+bestlam = cv.out$lambda.min
+lasso.pred = predict(lasso.mod, s=bestlam, newx=x[test, ])
 mean((lasso.pred-y.test)^2)
-out=glmnet(x,y,alpha=1,lambda=grid)
-lasso.coef=predict(out,type="coefficients",s=bestlam)[1:20,]
+out = glmnet(x, y, alpha=1, lambda=grid)
+lasso.coef=predict(out, type="coefficients", s=bestlam)[1:20, ]
 lasso.coef
 lasso.coef[lasso.coef!=0]
 
@@ -165,25 +165,25 @@ lasso.coef[lasso.coef!=0]
 # РЕГРЕССИЯ НА ГЛАВНЫЕ КОМПОНЕНТЫ
 library(pls)
 set.seed(2)
-pcr.fit=pcr(Salary~., data=Hitters,scale=TRUE,validation="CV")
+pcr.fit = pcr(Salary~., data=Hitters, scale=TRUE, validation="CV")
 summary(pcr.fit)
-validationplot(pcr.fit,val.type="MSEP")
+validationplot(pcr.fit, val.type="MSEP")
 set.seed(1)
-pcr.fit=pcr(Salary~., data=Hitters,subset=train,scale=TRUE, validation="CV")
-validationplot(pcr.fit,val.type="MSEP")
-pcr.pred=predict(pcr.fit,x[test,],ncomp=7)
+pcr.fit = pcr(Salary~., data=Hitters, subset=train, scale=TRUE, validation="CV")
+validationplot(pcr.fit, val.type="MSEP")
+pcr.pred = predict(pcr.fit, x[test, ], ncomp=7)
 mean((pcr.pred-y.test)^2)
-pcr.fit=pcr(y~x,scale=TRUE,ncomp=7)
+pcr.fit = pcr(y~x, scale=TRUE, ncomp=7)
 summary(pcr.fit)
 
 # Partial Least Squares
 # РЕГРЕССИЯ ПО МЕТОДУ ЧАСТНЫХ НАИМЕНЬШИХ КВАДРАТОВ
 set.seed(1)
-pls.fit=plsr(Salary~., data=Hitters,subset=train,scale=TRUE, validation="CV")
+pls.fit = plsr(Salary~., data=Hitters, subset=train, scale=TRUE, validation="CV")
 summary(pls.fit)
-validationplot(pls.fit,val.type="MSEP")
-pls.pred=predict(pls.fit,x[test,],ncomp=2)
+validationplot(pls.fit, val.type="MSEP")
+pls.pred = predict(pls.fit, x[test, ], ncomp=2)
 mean((pls.pred-y.test)^2)
-pls.fit=plsr(Salary~., data=Hitters,scale=TRUE,ncomp=2)
+pls.fit = plsr(Salary~., data=Hitters, scale=TRUE, ncomp=2)
 summary(pls.fit)
 
